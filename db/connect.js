38708c1079ff4c86b84e92d00a9ece66825f27b2
Aditya Das <aditya.das@tlcgroup.net>
2022-09-16 17:14:19 +0530

    crediential changed

--- db/connect.js
+++ db/connect.js
@@ -9,12 +9,12 @@ require("dotenv").config();
 //   port: process.env.PG_PORT,
 // });
 
-// const pool = new Pool({
-//   user: "hwseyuwwbcuiqj",
-//   host: "ec2-18-208-55-135.compute-1.amazonaws.com",
-//   database: "d9ucbkq5jjugu6",
-//   password: "28dd7b3dbf549978db39afc47b23c95482235b10d2291bde91c099f911524f13",
-//   port: 5432,
-// });
+const pool = new Pool({
+  user: "hwseyuwwbcuiqj",
+  host: "ec2-18-208-55-135.compute-1.amazonaws.com",
+  database: "d9ucbkq5jjugu6",
+  password: "28dd7b3dbf549978db39afc47b23c95482235b10d2291bde91c099f911524f13",
+  port: 5432,
+});
 
 module.exports = pool;
