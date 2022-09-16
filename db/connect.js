ffb603a9738be21c1af802cfbf2cec7d49e022bf
Aditya Das <aditya.das@tlcgroup.net>
2022-09-16 13:24:40 +0530

    env updated

--- db/connect.js
+++ db/connect.js
@@ -1,11 +1,12 @@
 const Pool = require("pg").Pool;
+require("dotenv").config();
 
 const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "todo",
-  password: "Aditya77*",
-  port: 5432,
+  user: process.env.PG_USER,
+  host: process.env.PG_ENDPOINT,
+  database: process.env.PG_DB,
+  password: process.env.PG_PASS,
+  port: process.env.PG_PORT,
 });
 
 module.exports = pool;
