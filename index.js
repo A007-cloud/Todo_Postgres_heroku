5e135ddce904f9d538d6d1d7ec076bba4393a60d
Aditya Das <aditya.das@tlcgroup.net>
2022-09-16 12:44:56 +0530

    environment variable added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const pool = require("./db/connect");
+require("dotenv").config();
 const bodyParser = require("body-parser");
 const todoRouter = require("./routes/todo");
 const notFoundMiddleware = require("./middleware/not-found");
@@ -23,10 +24,8 @@ app.use("/api/v1/todo", todoRouter);
 
 app.use(notFoundMiddleware);
 
-const PORT = 3000;
+const port = process.env.PORT;
 
-console.log(`Port: ${PORT}`);
-
-app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
+app.listen(port, () => {
+  console.log(`Server listening .....`);
 });
