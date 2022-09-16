00b43334dbcef403fe04cb61b4209a2843a2a5bf
Aditya Das <aditya.das@tlcgroup.net>
2022-09-16 14:23:05 +0530

    postgres connect

--- db/connect.js
+++ db/connect.js
@@ -1,12 +1,20 @@
 const Pool = require("pg").Pool;
 require("dotenv").config();
 
+// const pool = new Pool({
+//   user: process.env.PG_USER,
+//   host: process.env.PG_ENDPOINT,
+//   database: process.env.PG_DB,
+//   password: process.env.PG_PASS,
+//   port: process.env.PG_PORT,
+// });
+
 const pool = new Pool({
-  user: process.env.PG_USER,
-  host: process.env.PG_ENDPOINT,
-  database: process.env.PG_DB,
-  password: process.env.PG_PASS,
-  port: process.env.PG_PORT,
+  user: "hwseyuwwbcuiqj",
+  host: "ec2-18-208-55-135.compute-1.amazonaws.com",
+  database: "d9ucbkq5jjugu6",
+  password: "28dd7b3dbf549978db39afc47b23c95482235b10d2291bde91c099f911524f13",
+  port: 5432,
 });
 
 module.exports = pool;
