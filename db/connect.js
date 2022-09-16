28a9fb0e6393bff81c0ca835ae9c5ef9a3077865
Aditya Das <aditya.das@tlcgroup.net>
2022-09-16 17:11:15 +0530

    crediential changed

--- db/connect.js
+++ db/connect.js
@@ -1,13 +1,13 @@
 const Pool = require("pg").Pool;
 require("dotenv").config();
 
-const pool = new Pool({
-  user: process.env.PG_USER,
-  host: process.env.PG_ENDPOINT,
-  database: process.env.PG_DB,
-  password: process.env.PG_PASS,
-  port: process.env.PG_PORT,
-});
+// const pool = new Pool({
+//   user: process.env.PG_USER,
+//   host: process.env.PG_ENDPOINT,
+//   database: process.env.PG_DB,
+//   password: process.env.PG_PASS,
+//   port: process.env.PG_PORT,
+// });
 
 // const pool = new Pool({
 //   user: "hwseyuwwbcuiqj",
