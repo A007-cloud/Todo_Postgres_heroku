e4247b695fb517e39c4bb3c831090113b8e4abf4
Aditya Das <aditya.das@tlcgroup.net>
2022-09-19 13:08:59 +0530

    database url setup

--- db/connect.js
+++ db/connect.js
@@ -1,20 +1,28 @@
 const Pool = require("pg").Pool;
 require("dotenv").config();
 
+const devConfig = {
+  user: process.env.PG_USER,
+  host: process.env.PG_ENDPOINT,
+  database: process.env.PG_DB,
+  password: process.env.PG_PASS,
+  port: process.env.PG_PORT,
+};
+
 // const pool = new Pool({
-//   user: process.env.PG_USER,
-//   host: process.env.PG_ENDPOINT,
-//   database: process.env.PG_DB,
-//   password: process.env.PG_PASS,
-//   port: process.env.PG_PORT,
+//   user: "hwseyuwwbcuiqj",
+//   host: "ec2-18-208-55-135.compute-1.amazonaws.com",
+//   database: "d9ucbkq5jjugu6",
+//   password: "28dd7b3dbf549978db39afc47b23c95482235b10d2291bde91c099f911524f13",
+//   port: 5432,
 // });
 
-const pool = new Pool({
-  user: "hwseyuwwbcuiqj",
-  host: "ec2-18-208-55-135.compute-1.amazonaws.com",
-  database: "d9ucbkq5jjugu6",
-  password: "28dd7b3dbf549978db39afc47b23c95482235b10d2291bde91c099f911524f13",
-  port: 5432,
-});
+const proConfig = {
+  connectionString: process.env.DATABASE_URL, //HEROKU-ADDONSs
+};
+
+const pool = new Pool(
+  process.env.NODE_ENV === "production" ? proConfig : devConfig
+);
 
 module.exports = pool;
